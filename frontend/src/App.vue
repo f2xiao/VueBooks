6eb4e970242d57de18c62fa25bfc7e8d10defcf0
f2xiao <fxiao.mecheng@gmail.com>
2022-04-25 22:58:33 -0400

    Add nav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/books">Books</router-link>|
+    <router-link to="/categories">Categories</router-link>|
+    <router-link to="/about">About</router-link>|
+    <router-link to="/signin">Sign in</router-link>|
+    <router-link to="/signup">Sign up</router-link>
   </nav>
   <router-view/>
 </template>
@@ -11,7 +15,6 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 
