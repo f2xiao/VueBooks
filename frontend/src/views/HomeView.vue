52f731ea1fb38c5a4ef871f9a891cf8b17534502
f2xiao <fxiao.mecheng@gmail.com>
2022-04-25 23:01:17 -0400

    Delete HelloWorld component

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <HelloWorld/>
+     <h1>Home</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+   
   }
 }
 </script>
